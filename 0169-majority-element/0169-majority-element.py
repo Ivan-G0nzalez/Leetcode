a6cd5d574c2974416c7bffee660bd66743ecb48d
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-09-03 00:46:14 -0500

    Time: 109 ms (97.80%), Space: 14.9 MB (74.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,4 +5,17 @@ class Solution(object):
         :rtype: int
         """
         
-        return sorted(nums)[len(nums) // 2]
+        result = 0
+        counter = 0
+
+
+        for num in nums:
+            if counter == 0:
+                result = num
+
+            if num == result:
+                counter += 1
+            else:
+                counter -= 1
+
+        return result        
