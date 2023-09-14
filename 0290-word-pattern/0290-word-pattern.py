d9164e8e714ef0e9d0d51161b84faab25c632406
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-09-14 15:34:19 -0500

    Time: 37 ms (66.78%), Space: 16.3 MB (72.06%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,23 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split(" ")
+    
+        if len(words) != len(pattern): 
+            return False
+
+        word_map = {}
+        char_map = {}
+
+
+        for i in range(len(words)):
+            word = words[i]
+            char = pattern[i] 
+
+            if word not in word_map and char not in char_map:
+                word_map[word] = char
+                char_map[char] = word
+
+            elif word != char_map.get(char) or char != word_map.get(word):
+                return False
+
+        return True
