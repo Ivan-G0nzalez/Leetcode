de5b8b9dcc8aa3a3cdc3a729ebb1c9f968b8e3a0
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-09-15 11:51:50 -0500

    Time: 83 ms (84.25%), Space: 64.8 MB (30.99%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -1,14 +1,15 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
   let mapper = new Map();
 
-  for (let i = 0; i < nums.length; i++) {
-    if (!mapper.has(nums[i])) {
-      mapper.set(nums[i], i);
-    } else if (i - mapper.get(nums[i]) <= k) {
-      return true;
-    } else {
-      mapper.set(nums[i], i);
-    }
+    
+  for (let i = 0; i< nums.length; i++){
+      const currentValue = nums[i]
+      if (mapper.has(currentValue) && i - mapper.get(currentValue) <= k){
+          return true
+      }
+      else{
+          mapper.set(currentValue, i)
+      }
   }
-  return false;
-};
+    return false;
+}
