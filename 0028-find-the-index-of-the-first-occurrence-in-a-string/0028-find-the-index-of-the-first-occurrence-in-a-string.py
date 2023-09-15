503311f751c1431c5142299ebb8d723f12a59577
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-09-15 17:41:18 -0500

    Time: 20 ms (19.62%), Space: 13.2 MB (77.79%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -8,7 +8,7 @@ class Solution(object):
         text_len = len(haystack)
         substring_len = len(needle)
 
-        for i in range(text_len):
+        for i in range((text_len - substring_len) + 1):
             if haystack[i: i + substring_len] == needle:
                 return i
 
