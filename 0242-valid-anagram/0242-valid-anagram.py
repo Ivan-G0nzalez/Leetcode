b776a596bf75aa91abd62898a2a82f99bfafe6f3
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-10-03 15:34:22 -0500

    Time: 39 ms (39.58%), Space: 13.5 MB (99.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,18 +7,4 @@ class Solution(object):
         """
         
         
-        if len(s) != len(t):
-            return False
-        
-        count_s, count_t = {}, {}
-        
-        
-        for index in range(len(s)):
-            count_s[s[index]] = 1 + count_s.get(s[index], 0)
-            count_t[t[index]] = 1 + count_t.get(t[index], 0)
-        
-        for counter in count_s:
-            if count_s[counter] != count_t.get(counter, 0):
-                return False
-        
-        return True
+        return Counter(s) == Counter(t)
