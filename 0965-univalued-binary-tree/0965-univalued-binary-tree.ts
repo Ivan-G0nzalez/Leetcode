9ef3c1cbe4e62861fc5975041a9b82f3f8e71b86
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-10-24 14:06:00 -0500

    Time: 69 ms (6.82%), Space: 44.8 MB (29.55%) - LeetHub

---
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isUnivalTree(root: TreeNode | null): boolean {
+    function dfs(root){
+        if (!root){
+        return true
+        }
+
+        if (root.right !== null && root.val !== root.right.val){
+            return false
+        }
+
+        if (root.left !== null && root.val !== root.left.val){
+             return false
+         }
+
+        return dfs(root.right) && dfs(root.left)
+    }
+    
+    
+    return dfs(root)
+};
