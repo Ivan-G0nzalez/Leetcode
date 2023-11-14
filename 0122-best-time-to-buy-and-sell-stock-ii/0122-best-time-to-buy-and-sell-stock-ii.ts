448f48feed737117abc4ba29e57fcc12b1aa3e6d
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-11-14 15:22:00 -0500

    Time: 60 ms (49.39%), Space: 45.3 MB (7.20%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.ts
@@ -0,0 +1,28 @@
+function maxProfit(prices: number[]): number {
+    if (prices.length === 0){
+        return 0
+    }
+    
+    let bestTimeToBuy = prices[0];
+    let bestTimeToSell = prices[0];
+    let previousProfix = 0
+    let profix = 0
+    
+    for (const day of prices){
+        if (day < bestTimeToBuy){
+            bestTimeToBuy = day
+            bestTimeToSell = day
+        }
+        
+        else if (day > bestTimeToSell){
+            bestTimeToSell = day
+            let currentProfix = bestTimeToSell - bestTimeToBuy;
+            
+            profix += currentProfix
+            bestTimeToBuy = day
+            
+        }
+    }
+    
+    return profix;
+};
