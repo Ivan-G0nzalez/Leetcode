09e03f92a5ebcb43a2fed0bbfc3c3762f56f5436
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-12-19 17:59:41 -0500

    Time: 6 ms (24.06%), Space: 44.4 MB (7.30%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,26 +1,25 @@
 class Solution {
     public int romanToInt(String s) {
         
-        Map<Character, Integer> roman_number = new HashMap<>();
+        Map<Character, Integer> romanNumber = new HashMap<>();
+        romanNumber.put('I', 1);
+        romanNumber.put('V', 5);
+        romanNumber.put('X', 10);
+        romanNumber.put('L', 50);
+        romanNumber.put('C', 100);
+        romanNumber.put('D', 500);
+        romanNumber.put('M', 1000);
         
-        roman_number.put('I', 1);
-        roman_number.put('V', 5);
-        roman_number.put('X', 10);
-        roman_number.put('L', 50);
-        roman_number.put('C', 100);
-        roman_number.put('D', 500);
-        roman_number.put('M', 1000);
+        int total = 0;
         
-        int result = 0;
-            
-        for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && roman_number.get(s.charAt(i)) < roman_number.get(s.charAt(i+1))) {
-                result -= roman_number.get(s.charAt(i));
-            } else {
-                result += roman_number.get(s.charAt(i));
+        for (int i = 0; i < s.length(); i++){
+            if ((i < s.length() - 1) && romanNumber.get(s.charAt(i)) < romanNumber.get(s.charAt(i+1))){
+                total -= romanNumber.get(s.charAt(i));
+            }
+            else {
+                total += romanNumber.get(s.charAt(i));
             }
         }
-        
-        return result;
+        return total;
     }
 }
