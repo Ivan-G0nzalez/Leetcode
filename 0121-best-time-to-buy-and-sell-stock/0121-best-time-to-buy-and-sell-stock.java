2961f5418c78f583208f44de25cf338bd531a761
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-12-19 11:29:26 -0500

    Time: 2 ms (75.41%), Space: 61.6 MB (15.05%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int bestDayToBuy = prices[0];
+        int bestDayToSell = prices[0];
+        
+        int maxProfix = 0;
+        
+        for (int i=1; i<prices.length; i++){
+            if (prices[i] < bestDayToBuy){
+                bestDayToBuy = prices[i];
+                bestDayToSell = prices[i];
+            }
+            else if (prices[i] >  bestDayToBuy){
+                bestDayToSell = prices[i];
+                int tempProfix = bestDayToSell - bestDayToBuy;
+                if (tempProfix > maxProfix){
+                    maxProfix = tempProfix;
+                }
+            }
+        }
+        
+        return maxProfix;
+    }
+}
