f53ff2ebc83d1f01c1f134043ec08cdbeb5b4dee
Ivan-G0nzalez <50751301+Ivan-G0nzalez@users.noreply.github.com>
2023-12-20 09:35:22 -0500

    Time: 4 ms (66.81%), Space: 44.4 MB (7.30%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,23 +1,35 @@
 class Solution {
+    private int translate(Character character){
+        switch(character){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
+    }
+    
     public int romanToInt(String s) {
         
-        Map<Character, Integer> romanNumber = new HashMap<>();
-        romanNumber.put('I', 1);
-        romanNumber.put('V', 5);
-        romanNumber.put('X', 10);
-        romanNumber.put('L', 50);
-        romanNumber.put('C', 100);
-        romanNumber.put('D', 500);
-        romanNumber.put('M', 1000);
         
         int total = 0;
         
         for (int i = 0; i < s.length(); i++){
-            if ((i < s.length() - 1) && romanNumber.get(s.charAt(i)) < romanNumber.get(s.charAt(i+1))){
-                total -= romanNumber.get(s.charAt(i));
+            if ((i < s.length() - 1) && translate(s.charAt(i)) < translate(s.charAt(i+1))){
+                total -= translate(s.charAt(i));
             }
             else {
-                total += romanNumber.get(s.charAt(i));
+                total += translate(s.charAt(i));
             }
         }
         return total;
